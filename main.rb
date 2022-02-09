10718d875a963315b0b115a1892612e0e3d468da
Josef-Gustafsson-nti-johanneberg <josef.gustafsson@elev.ga.ntig.se>
2022-02-09 11:38:56 +0100

    Fixat en sida där man registrerar användare, samt se till att det fungerar med databasen

--- main.rb
+++ main.rb
@@ -17,21 +17,45 @@ get('/showlogin') do
     slim(:login)
 end
 
+get('/register') do
+  slim(:register)
+end
+
+post('/users/new'){
+  username = params[:username]
+  password = params[:password]
+  password_confirm = params[:password_confirm]
+  klass = params[:klass]
+  role = params[:role]
+  p role
+
+  if (password == password_confirm)
+    password_digest = BCrypt::Password.create(password)
+    db = SQLite3::Database.new('db/databas.db')
+    db.execute('INSERT INTO users (user_name,password,role) VALUES (?,?,?)',username,password_digest,role)
+    redirect('/')
+  else
+    "lösenorden matchade inte"
+  end
+
+}
+
+
 post('/login'){
-    username = params[:username]
+    user_name = params[:user_name]
     password = params[:password]
     
-    # db = SQLite3::Database.new('db/databas.db')
-    # db.results_as_hash = true
-    # result = db.execute('SELECT * FROM users WHERE username = ?',username).first
-    # pwdigest = result['pwdigest']
-    # id = result['id']
-    # if BCrypt::Password.new(pwdigest) == password
-    #   session[:id] = id
+    db = SQLite3::Database.new('db/databas.db')
+    db.results_as_hash = true
+    result = db.execute('SELECT * FROM users WHERE user_name = ?',user_name).first
+    pwdigest = result['password']
+    id = result['id']
+    if BCrypt::Password.new(pwdigest) == password
+      session[:id] = id
       redirect('/show')
-    # else
-    #   "Fel lösenord"
-    # end
+    else
+      "Fel lösenord"
+    end
   
   }
 
