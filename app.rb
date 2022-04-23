187c79122d86fe29e23b9354178d74becd6a77d4
Josef-Gustafsson-nti-johanneberg <josef.gustafsson@elev.ga.ntig.se>
2022-04-23 14:23:19 +0200

    massa yardoc

--- app.rb
+++ app.rb
@@ -41,6 +41,7 @@ end
 
 #Visar förstasidan på school-food som visar upp appa resturanger
 #
+# @see Model#select_all_from_resturants
 get('/') do
   result = select_all_from_resturants()
   slim(:index,locals:{resturants:result})
@@ -53,10 +54,12 @@ get('/showlogin') do
   slim(:login,locals:{text:text})
 end
 
-# Loggar in användaren om användaren skriver in rätt användarnamn och lösenord. Om användaren skriver in ett användarnamn som inte finns så kommer ett felmedelande att det itne finns och om användaren skriver in fel lösenord som kommer också ett felmeddelande om det
+# Loggar in användaren om användaren skriver in rätt användarnamn och lösenord. Om användaren skriver in ett användarnamn som inte finns så kommer ett felmedelande att det itne finns och om användaren skriver in fel lösenord som kommer också ett felmeddelande om det. Om användaren skriver fel lösenord för många gånger så måste hen vänta 10 sekunder innan nästa försök.
 # 
 # @param [String] username, Användarnamnet som användaren skrev in
 # @param [String] password, Lösenordet som användaren skrev in
+# @see Model#get_all_from_user
+# @See#cooldown
 post('/login')do
   time = Time.now.to_i
   input_user_name = params[:user_name].downcase
@@ -87,21 +90,24 @@ post('/login')do
   slim(:login,locals:{text:text})
 end
 
+# Tömmer sessions när man loggar ut
+# 
 get('/destroy')do
   session.destroy
   redirect('/')
 end
 
-
-
-# recenssioner
+# En sida som visa alla recensioner om man är admin eller visar bara de recensioner som man själv har skrivit.
+# 
+# @see Model#select_all_from_recenssioner
+# @see Model#role
 get('/recenssioner/'){
   if role(session[:id]) == "Admin"
     result = select_all_from_recenssioner(nil, nil)
     header = "Alla recensioner"
     slim(:'recenssion/index',locals:{recenssioner:result, title:header})
     elsif session[:id] != nil
-      result = select_all_from_recenssioner(session[:id])
+      result = select_all_from_recenssioner("user_id",session[:id])
       header = "Dina recensioner"
       slim(:'recenssion/index',locals:{recenssioner:result, title:header})
     else
@@ -109,8 +115,10 @@ get('/recenssioner/'){
   end
 }
 
-
-
+# Sidan där man kan skapa nya recenssioner om man är inloggad
+# 
+# @see Model#select_name_from_resturants
+# @see Model#role
 get('/recenssioner/new'){
   if role(session[:id]) != nil
     result = select_name_from_resturants
@@ -120,7 +128,17 @@ get('/recenssioner/new'){
   end
 }
 
-
+# Skapar den recenssion som användaren vill göra
+# 
+# @param [string] :resturang, vilken resturang användaren recenserar
+# @param [string] :title, titlen på recenssionen
+# @param [string] :recenssion, själva brödtexten i recenssionen
+# @param [integer] :rating, Ett tal mellan ett och fem som bedömmer resturangen
+# @param [integer] :id, användarid som skapade recensionen
+# @param [string] :image, bilden man laddade upp till recensionen
+# @param [string] :filename, filnamnet på bilden som laddedes upp
+# @param [string] :tempfile, är vart den bilden man ladde upp ligger på datort temporät
+# @see Model#incert_into_recenssioner
 post('/recenssioner'){
   resturang = params[:resturang]
   titel = params[:titel]
@@ -140,6 +158,12 @@ post('/recenssioner'){
   redirect('/recenssioner/') 
 }
 
+# Sidan där man kan redigera en recenssion
+# 
+# @param [string] :recenssion, är namnet på den recensison som man vill redigera
+# @see Model#right_persson
+# @see Model#select_all_from_recenssioner
+# @see Model#role
 get('/recenssion/:recenssion/edit')do
   text = ""
   recenssion = params[:recenssion]
@@ -151,6 +175,16 @@ get('/recenssion/:recenssion/edit')do
     slim(:login,locals:{text:text})
   end
 end
+
+# Uppdaterar recenssionen i databasen
+# 
+# @param [integer] :id, recenssion id 
+# @param [string] :title, titlen på recenssionen
+# @param [string] :recenssion, själva brödtexten i recenssionen
+# @param [integer] :rating, Ett tal mellan ett och fem som bedömmer resturangen
+# @param [string] :bild, är sökvägen till bilden som användaren laddade upp
+# #see Model#select_all_from_recenssioner
+# #see Model#update_recenssion
 
 post('/recenssion/:id/update')do
   id = params[:id]
@@ -169,6 +203,12 @@ post('/recenssion/:id/update')do
   end
 end
 
+# Tar bort en recenssion och leder användaren till recenssions sidan, om användaren inte får ta bort den så omleds användaren till login sidan
+# 
+# @param [string] :recenssion, är titlen på recenssionen
+# @see Model#right_persson
+# @see Model#delete_recenssion
+# @see Model#role
 post('/recenssion/:recenssion/delete')do
   recenssion = params[:recenssion]
   if right_persson(recenssion, session[:id]) || role(session[:id])
@@ -180,12 +220,22 @@ post('/recenssion/:recenssion/delete')do
   end
 end
 
-#users
+# Visar sidan där man kan skapa nya användare
+# 
 get('/users/new') do
   text = ""
   slim(:'users/new',locals:{text:text})
 end
 
+# Skapar en ny användare och skickar användaren till startsidan som inloggad, om användarnamnet redan finns så kommer ett felmedelande att det redan finns, om lösenorden inte stämmer överens så kommer också ett felmedelande om att de inte matchar
+# 
+# @param [string] :username, användarnamnet man skrev in, till små bokstäver så att det inte är versal känsligt
+# @param [string] :password, lösenordet som använderen skrev in
+# @param [string] :password_confirm, upprepade lösenordet som användaren skrev in
+# @param [string] :klass, klassen om användren klickade i
+# @param [string] :role, rollen som anvnädaren fick välja
+# @see Model#get_all_from_user
+# @see Model#incert_into_user
 post('/users/new')do
   username = params[:username].downcase
   password = params[:password]
@@ -206,16 +256,27 @@ post('/users/new')do
   end
 end
 
-# Resturanger
+# Skapar en resturan med anvädar input data och leder till startsidan
+# 
+# @param [string] :resturant, namnet på resturangen
+# @param [string] :beskrivning, en beskrivning på resturangen
+# @param [integer] :amount, hur många kategorier som användaren lägger till på resturangen
+# @param [integer] :catagory, ett kattegori id
+# @param [integer] :catagory1, ett kattegori2 id
+# @param [integer] :catagory2, ett kattegori3 id
+# @param [string] :plats, vart resturangen ligger
+# @param [string] :image, bilden man laddade upp till resturangen
+# @param [string] :filename, filnamnet på bilden som laddedes upp
+# @param [string] :tempfile, är vart den bilden man ladde upp ligger på datort temporät
+# @see Model#incert_into_resturants 
+# @see Model#get_id_from_resturants
+# @see Model#incert_into_resturnat_category_relation
 post('/resturant')do
   resturant = params[:resturant]
   beskrivning = params[:beskrivning]
   antal = params[:amount].to_i
-  kategori = params[:sak]
+  kategori = params[:catagory]
   plats = params[:plats]
-  bild = params[:bild]
-  db = open_database
-
   if params[:image] && params[:image][:filename]
     filename = params[:image][:filename]
     file = params[:image][:tempfile]
@@ -225,48 +286,60 @@ post('/resturant')do
     end
   end
   path = "/uploaded_pictures/#{filename}"
-  db.execute('INSERT INTO resturants (name,location,picture,description) VALUES (?,?,?,?)',resturant,plats,path,beskrivning)
-  id = db.execute('SELECT id FROM resturants WHERE name = ?',resturant).first
-  db.execute('INSERT INTO resturnat_category_relation(resturant_id,category_id) VALUES (?,?)',id["id"],kategori)
+  incert_into_resturants(resturant,plats,path,beskrivning)
+  id = get_id_from_resturants(resturant)
+  incert_into_resturnat_category_relation(id["id"],kategori)
   if antal == 2
     kategori1 = params[:catagory1]
-    db.execute('INSERT INTO resturnat_category_relation(resturant_id,category_id) VALUES (?,?)',id["id"],kategori1)
+    incert_into_resturnat_category_relation(id["id"],kategori1)
   elsif antal == 3
     kategori1 = params[:catagory1]
-    db.execute('INSERT INTO resturnat_category_relation(resturant_id,category_id) VALUES (?,?)',id["id"],kategori1)
+    incert_into_resturnat_category_relation(id["id"],kategori1)
     kategori2 = params[:catagory2]
-    db.execute('INSERT INTO resturnat_category_relation(resturant_id,category_id) VALUES (?,?)',id["id"],kategori2)
+    incert_into_resturnat_category_relation(id["id"],kategori2)
   end
   redirect('/')
-  
 end
 
+# Sida som visar upp alla resturanger för admin så admin kan redigera och ta bort resturanger
+# 
+# @see Model#select_all_from_resturants
+# @see Model#role
 get('/resturant/')do
   if role(session[:id]) == "Admin"
-    db = open_database
-    result = db.execute('SELECT * FROM resturants')
+    result = select_all_from_resturants()
     slim(:'resturants/index',locals:{resturants:result})
   else
     redirect('/showlogin')
   end
 end
+
+# Sida där man kan skapa nya resturanger om man är admin
+# @see Model#role
 get('/resturant/new')do
-  if role(session[:id]) != nil
+  if role(session[:id]) == "Admin"
     slim(:'resturants/new')
   else
     redirect('/showlogin')
   end
 end
 
+# En sida som visar upp en spesefic resturang samt tillhörande recenssioner.
+# 
+# @params [string] :resturant, resturang namnet
+# @see Model#select_all_from_resturants_where_resturant_name
+# @see Model#select_all_from_recenssioner
 get('/resturant/:resturant')do
-  db = open_database
   resturant = params[:resturant]
-  ratings = db.execute('SELECT * FROM recenssioner WHERE resturant=?', resturant)
-  result = db.execute('SELECT * FROM resturants WHERE name = ?', resturant).first
+  result = select_all_from_resturants_where_resturant_name(resturant)
+  ratings = select_all_from_recenssioner("resturant", resturant)
   slim(:'resturants/show',locals:{resturant:result, ratings:ratings})
 end
 
-
+# Tar bort en resturang och skickar användaren till resturang sidan
+# 
+# @params [string] :resturant, resturang namnet
+# @see Model#delete_resturant
 post('/resturant/:resturant/delete')do
   resturant = params[:resturant]
   delete_resturant(resturant)
--- model/model.rb
+++ model/model.rb
@@ -6,9 +6,52 @@ module Model
         return db
     end
 
+    def password_match(pwdigest, password)
+        return BCrypt::Password.new(pwdigest) == password
+    end
+
+    def role(id)
+        db = open_database
+        result = db.execute('SELECT role FROM users WHERE id = (?)',id).first
+        if result == nil
+            return nil
+        else
+            return result["role"]
+        end
+    end
+
     def select_all_from_resturants
         db = open_database
         return db.execute('SELECT * FROM resturants')
+    end
+
+    def select_all_from_resturants_where_resturant_name(name)
+        db = open_database
+        result = db.execute('SELECT * FROM resturants WHERE name = ?', name).first
+    end
+      
+    def get_all_from_user(input_user_name)
+        db = open_database
+        return db.execute('SELECT * FROM users WHERE user_name = (?)',input_user_name).first
+    end
+    
+    def select_all_from_recenssioner(condition, is)
+        db = open_database
+        if condition != nil
+            return db.execute("SELECT * FROM recenssioner WHERE #{condition} = ?",is)
+        else
+            return db.execute('SELECT * FROM recenssioner')
+        end
+    end
+
+    def select_name_from_resturants()
+        db = open_database
+        return db.execute('SELECT name FROM resturants')
+    end
+
+    def get_id_from_resturants(resturant)
+        db = open_database
+        return db.execute('SELECT id FROM resturants WHERE name = ?',resturant).first
     end
 
     def right_persson(recenssion, user_id)
@@ -17,10 +60,6 @@ module Model
         return recenssion_user_id["user_id"] == user_id
     end
 
-    def get_all_from_user(input_user_name)
-        db = open_database
-        return db.execute('SELECT * FROM users WHERE user_name = (?)',input_user_name).first
-    end
 
     def incert_into_users(klass,username,password,role)
         db = open_database
@@ -30,55 +69,36 @@ module Model
         return db.execute('SELECT id FROM users WHERE user_name = ?',username).first
     end
 
-    def select_all_from_recenssioner(condition, is)
+    def incert_into_recenssion(user_id,resturang,rating,path,titel,recenssion)
         db = open_database
-        if condition != nil
-        return db.execute("SELECT * FROM recenssioner WHERE #{condition} = ?",is)
-        else
-        return db.execute('SELECT * FROM recenssioner')
-        end
+        db.execute('INSERT INTO recenssioner (user_id,resturant,stars,picture,title,recenssion) VALUES (?,?,?,?,?,?)',user_id,resturang,rating,path,titel,recenssion)
     end
+
+    def incert_into_resturants(resturant,plats,path,beskrivning)
+        db = open_database
+        db.execute('INSERT INTO resturants (name,location,picture,description) VALUES (?,?,?,?)',resturant,plats,path,beskrivning)
+    end
+      
+    def incert_into_resturnat_category_relation(id,kategori)
+        db = open_database
+        db.execute('INSERT INTO resturnat_category_relation(resturant_id,category_id) VALUES (?,?)',id,kategori)
+    end
+
 
     def delete_resturant(resturant)
         db = open_database
         db.execute('DELETE FROM resturants WHERE name = ?',resturant)
     end
 
-    def select_name_from_resturants()
+    def delete_recensson(recenssion)
         db = open_database
-        return db.execute('SELECT name FROM resturants')
+        db.execute('DELETE FROM recenssioner WHERE title = ?',recenssion)
     end
 
-    def incert_into_recenssion(user_id,resturang,rating,path,titel,recenssion)
-        db = open_database
-        db.execute('INSERT INTO recenssioner (user_id,resturant,stars,picture,title,recenssion) VALUES (?,?,?,?,?,?)',user_id,resturang,rating,path,titel,recenssion)
-    end
-
-
-
-    def password_match(pwdigest, password)
-        return BCrypt::Password.new(pwdigest) == password
-    end
-
-    def role(id)
-        db = open_database
-        result = db.execute('SELECT role FROM users WHERE id = (?)',id).first
-        if result == nil
-        return nil
-        else
-        return result["role"]
-        end
-    end
-    # En komentar
     def innerjoin(id)
         db = open_database
         db.results_as_hash = false
         return db.execute('SELECT category.name FROM resturnat_category_relation INNER JOIN category ON resturnat_category_relation.category_id = category.id WHERE resturant_id = (?)',id)
-      end
-
-    def delete_recensson(recenssion)
-        db = open_database
-        db.execute('DELETE FROM recenssioner WHERE title = ?',recenssion)
     end
 
     def update_recenssion(rating,bild,title,recenssion,id)
